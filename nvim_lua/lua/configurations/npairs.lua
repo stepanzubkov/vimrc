e36beb92d4a86e09e731c98bad544a550dc34c3f
Степан Зубков <zubkovbackend@gmail.com>
2023-05-20 22:45:58 +0300

    Fix nvim autopairs

--- nvim_lua/lua/configurations/npairs.lua
+++ nvim_lua/lua/configurations/npairs.lua
@@ -3,14 +3,4 @@ local npairs = require('nvim-autopairs')
 npairs.enable()
 npairs.setup{
   disable_filetype = { "TelescopePrompt" },
-  fast_wrap = {
-    map = '<M-e>',
-    chars = { '{', '[', '(', '"', "'" },
-    pattern = [=[[%'%"%>%]%)%}%,]]=],
-    end_key = '$',
-    keys = 'qwertyuiopzxcvbnmasdfghjkl',
-    check_comma = true,
-    highlight = 'Search',
-    highlight_grey='Comment'
-  },
 }
